9076182050d89f00451570b4df5fc3c1c6fd0ad1
AceRNorth <acenorth79@gmail.com>
2025-11-20 15:49:29 +0000

    RemoveBuild

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/biology/zool0973/Dropbox/SpatialModelRepo
-# Build directory: /home/biology/zool0973/Dropbox/SpatialModelRepo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
